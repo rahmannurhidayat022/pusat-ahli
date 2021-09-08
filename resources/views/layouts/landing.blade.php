979cdf78a95c429d5a579e4c293b61e4c5a9f0ce
Rahman Nurhidayat <rahmannurhidayat022@gmail.com>
2021-09-08 14:21:23 +0700

    creted footer

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -540,5 +540,71 @@
                 </div>
             </div>
         </section>
+        <footer id="footer" class="border-top bg-purple pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="d-flex justify-content-center d-md-block col-sm-12 col-md-4">
+                        <img class="mb-3" width="141" height="123" src="{{asset('images/brand-footer.png')}}" alt="brand logo" />
+                    </div>
+                    <div class="d-flex flex-column align-items-center d-md-block col-sm-12 col-md-4">
+                        <h3 class="h5 text-gray-300">Office</h4>
+                        <address class="text-gray-500">
+                            PT. Pusat Ahli <br />
+                            Jl.Maju terus pantang rugi No 25 <br />
+                            Jakarta - Indonesia <br />
+                            customer.pusatahli@gmail.com
+                        </address>
+                    </div>
+                    <div class="d-flex flex-column align-items-center d-md-block col-sm-12 col-md-4">
+                        <h3 class="h5 text-gray-300">Sosial media</h4>
+                        <ul class="list-group list-group-flush mb-sm-5">
+                            <li class="list-group-item">
+                                <i class="fab fa-instagram"></i>
+                                pusatahli
+                            </li>
+                            <li class="list-group-item">
+                                <i class="fab fa-facebook"></i>
+                                Pusat Ahli Indonesia
+                            </li>
+                            <li class="list-group-item">
+                                <i class="fab fa-youtube"></i>
+                                Pusat Ahli
+                            </li>
+                            <li class="list-group-item">
+                                <i class="fab fa-tiktok"></i>
+                                pusatahliid
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3 class="h5 text-gray-300">Layanan</h4>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Bidang ahli</li>
+                            <li class="list-group-item">Tempat ahli</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3 class="h5 text-gray-300">Perusahaan</h4>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Tentang</li>
+                            <li class="list-group-item">Kontak</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h3 class="h5 text-gray-300">Bahasa</h4>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Bahasa indonesia</li>
+                            <li class="list-group-item">English (US)</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="py-4 mx-auto border-top row d-flex flex-column align-items-center text-gray-600">
+                  &copy; Copyright Pusat Ahli. All Rights Reserved
+                  <div class="credits">
+                      Designed by <a class="text-gray-700" href="https://github.com/rahmannurhidayat022">Rahman Nurhidayat</a> | Front-End Pusat Ahli
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
