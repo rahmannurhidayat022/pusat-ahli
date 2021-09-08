c2e73ad494686078d493ca1754d9ae8e65322791
Rahman Nurhidayat <rahmannurhidayat022@gmail.com>
2021-09-08 10:04:19 +0700

    change to bootstrap original and created navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+<!-- <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
     <div class="container">
       <a class="navbar-brand js-scroll" href="#page-top"><img src="{{asset('frontend/img/Logo.png')}}"></a>
       <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
@@ -18,5 +18,27 @@
         </ul>
       </div>
     </div>
-  </nav>
+  </nav> -->
   <!--/ Nav End /-->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent shadow-sm">
+  <div class="container">
+  <a class="navbar-brand" href="#">
+    <img src="{{asset('frontend/img/Logo.png')}}" width="40" height="40" alt="brand logo">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FAQ</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,20 +1,17 @@
   <link href="{{asset('frontend/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Bootstrap CSS File -->
-  <link href="{{asset('frontend/lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-
-  <!-- Libraries CSS Files -->
+  <!-- <link href="{{asset('frontend/lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{asset('frontend/lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
   <link href="{{asset('frontend/lib/animate/animate.min.css')}}" rel="stylesheet">
   <link href="{{asset('frontend/lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
   <link href="{{asset('frontend/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet"> -->
+
+  <!-- Libraries CSS Files -->
 
   <!-- Main Stylesheet File -->
-  <link href="{{asset('frontend/css/style.css')}}" rel="stylesheet">
+  <!-- <link href="{{asset('frontend/css/style.css')}}" rel="stylesheet"> -->
   <link href="{{asset('css/app.css')}}" rel="stylesheet">
-<!-- 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"> -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
